f90c5cfe7934b5104d9d4efd689cdf0f1508a7e8
Amir Kidwai <findamirkidwai@gmail.com>
2024-04-06 14:43:52 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,35 @@
+#!pip install huggingface torch transformers
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/microsoft/phi-2"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
+
+print(output)
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+torch.set_default_device("cuda")
+
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+
+inputs = tokenizer('''def print_prime(n):
+   """
+   Print all primes between 1 and n
+   """''', return_tensors="pt", return_attention_mask=False)
+
+outputs = model.generate(**inputs, max_length=200)
+text = tokenizer.batch_decode(outputs)[0]
+print(text)
+
+print(model)
